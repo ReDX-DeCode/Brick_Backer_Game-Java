68e524a995c96d985eaefa1c76600fa907a19c01
Dinidu <disonic80@gmail.com>
2022-01-02 00:26:19 +0530

    Java Game

---
+++ src/brick_backer_game/Brick_Backer_Game.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brick_backer_game;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Admin
+ */
+public class Brick_Backer_Game {
+
+    public static void main(String[] args) {
+       
+        JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
+        obj.setBounds(10, 10, 710, 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        
+        
+    }
+    
+}
